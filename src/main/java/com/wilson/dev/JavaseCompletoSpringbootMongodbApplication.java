c263cfbcb0c1b18997e37a196045bc5aff5ae191
Wilson <wilsonprogram@gmail.com>
2023-07-12 07:23:22 -0300

    Projeto criado

---
+++ src/main/java/com/wilson/dev/JavaseCompletoSpringbootMongodbApplication.java
@@ -0,0 +1,13 @@
+package com.wilson.dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaseCompletoSpringbootMongodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaseCompletoSpringbootMongodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wilson/dev/JavaseCompletoSpringbootMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilson.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaseCompletoSpringbootMongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
