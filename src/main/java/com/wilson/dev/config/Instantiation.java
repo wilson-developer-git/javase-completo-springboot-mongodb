8c146db299ffd1adca9dfb2dabd306737e6798e8
Wilson <wilsonprogram@gmail.com>
2023-07-19 10:48:39 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/wilson/dev/config/Instantiation.java
+++ src/main/java/com/wilson/dev/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.wilson.dev.domain.Post;
 import com.wilson.dev.domain.User;
+import com.wilson.dev.dto.AuthorDTO;
 import com.wilson.dev.repository.PostRepository;
 import com.wilson.dev.repository.UserRepository;
 
@@ -34,11 +35,12 @@ public class Instantiation implements CommandLineRunner {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com");
 		User u3 = new User(null, "Bob Grey", "bob@gmail.com");
-
-		Post p1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", u1);
-		Post p2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", u1);
-		
 		userRepository.saveAll(Arrays.asList(u1, u2, u3));
+
+		Post p1 = new Post(null, sdf.parse("21/03/2018"), 
+				"Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(u1));
+		Post p2 = new Post(null, sdf.parse("21/03/2018"),
+				"Bom dia", "Acordei feliz hoje!", new AuthorDTO(u1));
 		postRepository.saveAll(Arrays.asList(p1, p2));
 	}
 
