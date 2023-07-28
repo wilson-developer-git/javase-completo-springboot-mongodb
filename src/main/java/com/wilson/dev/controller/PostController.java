9d8e6e4ef5c5cc735b0d8ad42ce8dea9d38630c7
Wilson <wilsonprogram@gmail.com>
2023-07-27 23:14:38 -0300

    Aula 354 - Consulta com vários critérios

--- src/main/java/com/wilson/dev/controller/PostController.java
+++ src/main/java/com/wilson/dev/controller/PostController.java
@@ -1,5 +1,6 @@
 package com.wilson.dev.controller;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -28,9 +29,37 @@ public class PostController {
 	}
 	
 	@GetMapping("/titlesearch")
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "", defaultValue = "") String text) {
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = postService.findByTitle(text);
 		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping("/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value = "text", defaultValue = "") String text,
+			@RequestParam(value = "minDate", defaultValue = "") String minDate,
+			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = postService.fullSearch(text, min, max);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 }
